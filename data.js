92d489baef954167c1e295a0ed44be8efe0a18bd
zachpatterson99 <zachpatterson99@gmail.com>
2024-07-05 16:22:48 -0400

    commit

---
+++ data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-location-6--n-university--fletcher",
+      "name": "Location #6- N University & Fletcher",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.042476552919655575,
+        "pitch": 0.023188153737820016,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.1818226707336654,
+          "pitch": -0.11180941909172049,
+          "title": "Emergency Location",
+          "text": "<h1 class=\"DUwDvf lfPIob\" style=\"background: rgb(255, 255, 255); border: 0px; border-radius: 0px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; font-size: 1.375rem; line-height: 1.75rem; font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; list-style: none; outline: 0px; overflow: visible; vertical-align: baseline; color: rgb(32, 33, 36);\">Michigan League</h1>"
+        }
+      ]
+    }
+  ],
+  "name": "Location #6 - N University & Fletcher",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
